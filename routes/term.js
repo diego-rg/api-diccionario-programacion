512659c578c9d84cbd896f986f31eeb59fafff1f
eu <diego_thegarden@hotmail.com>
2022-12-18 19:22:10 +0100

    migration form heroku to railway

--- routes/term.js
+++ routes/term.js
@@ -3,21 +3,19 @@ import termController from "../controllers/term.js";
 
 const router = express.Router();
 
-// router.post("/", (termController.createTerm));
+// router.post("/", termController.createTerm);
 
-router.get("/random", (termController.randomTerm));
+router.get("/random", termController.randomTerm);
 
-router.get("/all", (termController.allTerms));
+router.get("/all", termController.allTerms);
 
-router.get("/categories", (termController.termCategories));
+router.get("/categories", termController.termCategories);
 
-router.get("/:category", (termController.termCategory));
+router.get("/:category", termController.termCategory);
 
-router
-    .get("/:category/:name", (termController.termName))
-    // .put("/:category/:name", (termController.updateTerm))
-    // .delete("/:category/:name", (termController.destroyTerm));
-
+router.get("/:category/:name", termController.termName);
+// .put("/:category/:name", termController.updateTerm)
+// .delete("/:category/:name", termController.destroyTerm);
 
 export default router;
 
